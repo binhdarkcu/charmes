2a7afc10f0ae58928e5caa645bd4897c4fa27500
Binh <binhdarkcu@gmail.com>
2015-08-19 19:47:46 -0700

    aaaa

--- modules/includes/loader.php
+++ modules/includes/loader.php
@@ -1,48 +1,58 @@
-<div class="rotate">
-  <div class="dot dot1"></div>
-</div>
-<div class="rotate">
-  <div class="dot dot2"></div>
-</div>
-<div class="rotate">
-  <div class="dot dot3"></div>
-</div>
-<div class="rotate">
-  <div class="dot dot4"></div>
-</div>
-<div class="rotate">
-  <div class="dot dot5"></div>
-</div>
-<div class="rotate">
-  <div class="dot dot6"></div>
-</div>
-<div class="rotate">
-  <div class="dot dot7"></div>
-</div>
-<div class="rotate">
-  <div class="dot dot8"></div>
-</div>
-<div class="rotate">
-  <div class="dot dot9"></div>
-</div>
-<div class="rotate">
-  <div class="dot dot10"></div>
-</div>
-<div class="rotate">
-  <div class="dot dot11"></div>
-</div>
-<div class="rotate">
-  <div class="dot dot12"></div>
-</div>
-<div class="rotate">
-  <div class="dot dot13"></div>
-</div>
-<div class="rotate">
-  <div class="dot dot14"></div>
-</div>
-<div class="rotate">
-  <div class="dot dot15"></div>
-</div>
-<div class="rotate">
-  <div class="dot dot16"></div>
-</div>
+<div class="loader">Loading</div>
+<style>
+.loader {
+      width: 150px;
+    height: 150px;
+    line-height: 150px;
+    position: absolute;
+    box-sizing: border-box;
+    text-align: center;
+    z-index: 0;
+    text-transform: uppercase;
+    left: 50%;
+    margin-left: -75px;
+    top: 50%;
+    margin-top: -95px;
+    color: #fff;
+}
+
+.loader:before,
+.loader:after {
+  opacity: 0;
+  box-sizing: border-box;
+  content: "\0020";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 100px;
+  border: 5px solid #fff;
+  box-shadow: 0 0 50px #fff, inset 0 0 50px #fff;
+}
+
+.loader:after {
+  z-index: 1;
+  -webkit-animation: gogoloader 2s infinite 1s;
+}
+
+.loader:before {
+  z-index: 2;
+  -webkit-animation: gogoloader 2s infinite;
+}
+
+@-webkit-keyframes gogoloader {
+  0% {
+    -webkit-transform: scale(0);
+    opacity: 0;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    -webkit-transform: scale(1);
+    opacity: 0;
+  }
+}
+
+</style>
